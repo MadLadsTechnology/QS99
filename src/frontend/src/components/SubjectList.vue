d2ee4756eec676b443f0a9b47b78fd06c135f187
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-03-28 17:01:20 +0200

    Som additional work on frontend

---
+++ src/frontend/src/components/SubjectList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button>Øvinger</button> <button>kø</button>
+    <div class="container">
+      <h3>{{ subjectCode }}</h3>
+      {{ subjectName }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    subjectCode: String,
+    subjectName: String,
+    link: String,
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  width: 200px;
+  height: 200px;
+}
+
+a {
+  text-decoration: none;
+}
+
+h1 {
+  color: #f6f7eb;
+}
+h3 {
+}
+</style>
--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -3,21 +3,30 @@
 
   <h3>Here are your active subjects</h3>
 
-  <!-- ADD FOR LOOP TO LOOP THRU ALL SUBJECTS-->
-  <SubjectCard
-    subjectCode="IDATT1234"
-    subjectName="Testfag"
-    description=""
-    link="null"
-  />
+  <button @click="showAll = true">Alle fag</button>
+
+  <button>Fag 1</button>
+  <button>Fag 2</button>
+  <button>Fag 3</button>
+  <button>Fag 4</button>
+
+  <SubjectList v-if="showAll" />
+
+  <SubjectPage v-else />
 </template>
 
 <script>
 import { mapState } from "vuex";
-import SubjectCard from "@/components/SubjectCard";
+import SubjectList from "@/components/SubjectList";
+import SubjectPage from "@/components/SubjectPage";
 
 // @ is an alias to /src
 export default {
+  data() {
+    return {
+      showAll: true,
+    };
+  },
   computed: {
     ...mapState({
       isLoggedIn: "isLoggedIn",
@@ -25,6 +34,6 @@ export default {
     }),
   },
   name: "HomeView",
-  components: { SubjectCard },
+  components: { SubjectList, SubjectPage },
 };
 </script>

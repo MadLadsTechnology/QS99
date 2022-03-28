ff6cd4f37122c67b08a5d55e511a0c3e7ca1c9a3
Eivind Strand Harboe <eiha0403@gmail.com>
2022-03-28 10:22:29 +0200

    Initial Commit

---
+++ src/main/java/ntnu/idatt2105/madlads/FullstackAPI/FullstackApiApplication.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2105.madlads.FullstackAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ntnu/idatt2105/madlads/FullstackAPI/FullstackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2105.madlads.FullstackAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

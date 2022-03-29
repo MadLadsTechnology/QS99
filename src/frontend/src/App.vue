4772e8eca738688a66b0dec12055675b836239b6
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-03-29 16:31:55 +0200

    removed: axios interceptor

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -4,20 +4,17 @@
 <script>
 import axios from "axios";
 export default {
-  setup() {
+  created() {
     const userString = localStorage.getItem("user");
     if (userString) {
-      //const userData = JSON.parse(userString);
-      //this.$store.commit("SET_USER_DATA", userData);
+      const userData = JSON.parse(userString);
+      this.$store.commit("SET_USER_DATA", userData);
     }
 
     axios.interceptors.response.use(
       (response) => response,
       (error) => {
-        if (error.response.status === 401) {
-          this.$store.dispatch("logout");
-        }
-        return Promise.reject(error);
+        console.log(error);
       }
     );
   },

cf625a7523ea730a2eb9a6dca05abb6869fdc5e2
Eivind Strand Harboe <eiha0403@gmail.com>
2022-03-29 13:52:01 +0200

    Added: SubjectRepository

---
+++ src/main/java/ntnu/idatt2105/madlads/FullstackAPI/model/repositories/SubjectRepository.java
@@ -0,0 +1,14 @@
+package ntnu.idatt2105.madlads.FullstackAPI.model.repositories;
+
+import ntnu.idatt2105.madlads.FullstackAPI.model.subjects.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+
+    @Query("SELECT s.id, s.subjectName FROM Subject s, StudentSubject ss WHERE ss.id.emailAddress = :foundEmailAddress AND ss.subjectId = s.id")
+    Collection<Subject> findfindAllSubjectsByStudent(@Param("foundEmailAddress") String foundEmailAddress);
+}

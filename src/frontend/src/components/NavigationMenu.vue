68d084cd1c06368e149a1791de6a95397433e577
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-03-29 18:02:22 +0200

    Created: NavigationMenu.vue

---
+++ src/frontend/src/components/NavigationMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <router-link to="/">Subjects</router-link>
+
+    <button v-if="loggedIn" class="logOutBtn" @click="logOut()">Log out</button>
+  </div>
+</template>
+<script>
+import { authComputed } from "@/store/helpers";
+
+export default {
+  methods: {
+    logOut() {
+      this.$store.dispatch("logout");
+    },
+  },
+  computed: {
+    ...authComputed,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 30px;
+  height: 100px;
+  background-color: antiquewhite;
+  padding: 30px;
+}
+
+.logOutBtn {
+}
+</style>

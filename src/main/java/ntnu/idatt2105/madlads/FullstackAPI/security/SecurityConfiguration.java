0bef11183dcc38235ef1bc0d597fdbfc6f8ab66b
Anders Heftøy Carlsen <anders.heftoy@gmail.com>
2022-03-29 10:56:55 +0200

    Added: JWT Security

---
+++ src/main/java/ntnu/idatt2105/madlads/FullstackAPI/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package ntnu.idatt2105.madlads.FullstackAPI.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // token endpoint is not protected
+        http
+                .csrf().disable()
+                .cors().and()
+                .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .antMatchers(HttpMethod.GET, "/swagger-ui/index.html").permitAll()
+                .anyRequest().authenticated();
+    }
+}

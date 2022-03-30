7ffc0f5262dbbd8a21a6ee338a978ec0593ab26f
Eivind Strand Harboe <eiha0403@gmail.com>
2022-03-30 12:06:42 +0200

    Change: Now use dtype and not role

--- src/main/java/ntnu/idatt2105/madlads/FullstackAPI/model/repositories/UserRepository.java
+++ src/main/java/ntnu/idatt2105/madlads/FullstackAPI/model/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package ntnu.idatt2105.madlads.FullstackAPI.model.repositories;
 
 import ntnu.idatt2105.madlads.FullstackAPI.model.users.QSUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<QSUser, Long> {
     QSUser findByEmailAddress(String email);
@@ -10,5 +13,7 @@ public interface UserRepository extends JpaRepository<QSUser, Long> {
     @Query("")
     void changeRoleOfUserToAdmin(User user);*/
     //TODO: Add query for updating the users role
+
+    QSUser getDistinctByEmailAddress(String email);
 
 }

5dbce0af796e168ab317cf4c62c626579cdf8642
Eivind Strand Harboe <eiha0403@gmail.com>
2022-03-31 15:21:38 +0200

    Change: Security should now bed better

--- src/main/java/ntnu/idatt2105/madlads/FullstackAPI/security/SecurityConfiguration.java
+++ src/main/java/ntnu/idatt2105/madlads/FullstackAPI/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package ntnu.idatt2105.madlads.FullstackAPI.security;
 
 import ntnu.idatt2105.madlads.FullstackAPI.security.JWTAuthorization.JWTAuthorizationAdminFilter;
+import ntnu.idatt2105.madlads.FullstackAPI.security.JWTAuthorization.JWTAuthorizationProfessorFilter;
 import ntnu.idatt2105.madlads.FullstackAPI.security.JWTAuthorization.JWTAuthorizationUserFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -18,30 +19,49 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // token endpoint is not protected
+        //TODO: Make database console not accessible
         http.authorizeRequests().antMatchers("/h2-ui/**").permitAll()
             .and().csrf().ignoringAntMatchers("/h2-ui/**")
             .and().headers().frameOptions().sameOrigin();
 
-        http
-            .csrf().disable()
-            .cors().and()
-            .addFilterAfter(new JWTAuthorizationUserFilter(), UsernamePasswordAuthenticationFilter.class)
-            .authorizeRequests()
-            .antMatchers(HttpMethod.POST, "/user/**").permitAll()
-            .antMatchers(HttpMethod.DELETE, "/user/**").permitAll()
-            .antMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
-            .antMatchers(HttpMethod.POST, "/subject/**").authenticated()
-            .antMatchers(HttpMethod.GET,"/v3/**").authenticated()
-            .antMatchers(HttpMethod.POST,"/queue/**").permitAll()
-            .antMatchers(HttpMethod.POST, "/subject/**").permitAll()
-            .antMatchers(HttpMethod.GET,"/queue/**").authenticated()
-            .antMatchers(HttpMethod.POST, "/subject/**").permitAll()
-            .antMatchers(HttpMethod.GET, "/subject/**").permitAll();
+        http.csrf().disable()
+                .cors().and().authorizeRequests().antMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll();
+
+        http.authorizeRequests().antMatchers("/user/login").permitAll()
+                .and().csrf().ignoringAntMatchers("/user/login")
+                .and().headers().frameOptions().sameOrigin();
 
         http
-            .csrf().disable()
-            .cors().and()
-            .addFilterAfter(new JWTAuthorizationAdminFilter(), UsernamePasswordAuthenticationFilter.class)
-            .authorizeRequests() .antMatchers("/admin/**").authenticated();
+                .csrf().disable()
+                .cors().and()
+                .addFilterAfter(new JWTAuthorizationUserFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/user/login").permitAll()
+                .antMatchers(HttpMethod.GET,"/subject/getByUser").authenticated();
+
+        //PROFESSOR
+        http
+                .csrf().disable()
+                .cors().and()
+                .addFilterAfter(new JWTAuthorizationProfessorFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/user/registerStudent").authenticated()
+                .antMatchers(HttpMethod.POST,"/user/registerMultipleUsers").authenticated()
+                .antMatchers(HttpMethod.POST,"/subject/create").authenticated()
+                .antMatchers(HttpMethod.POST,"/subject/addStudent").authenticated()
+                .antMatchers(HttpMethod.POST,"/subject/addStudentAssistant").authenticated()
+                .antMatchers(HttpMethod.POST,"/subject/addProfessor").authenticated()
+                .antMatchers(HttpMethod.GET,"/subject/**").authenticated()
+                .antMatchers(HttpMethod.POST,"/queue/setQueueStatus").authenticated()
+                .antMatchers(HttpMethod.POST,"/queue/create").authenticated()
+                .antMatchers(HttpMethod.POST,"/user/login").authenticated()
+                .antMatchers(HttpMethod.GET,"/subject/getByUser").authenticated();
+
+        //ADMIN
+        http
+                .csrf().disable()
+                .cors().and()
+                .addFilterAfter(new JWTAuthorizationAdminFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests().antMatchers("/**").authenticated();
     }
 }

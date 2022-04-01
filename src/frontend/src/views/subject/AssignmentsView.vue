5eb0cfa1d6890381af52d44b53f32b826e131393
Eivind Strand Harboe <eiha0403@gmail.com>
2022-04-01 18:45:24 +0200

    Added: Assigment cards (NOTE SubjectId currently hardcoded)

--- src/frontend/src/views/subject/AssignmentsView.vue
+++ src/frontend/src/views/subject/AssignmentsView.vue
@@ -1,3 +1,44 @@
 <template>
-  <div></div>
+  <div>
+    <div class="cardHolder">
+      <AssignementCard
+        v-for="assignment in allAssignments"
+        :key="parseInt(assignment.id)"
+        :assignment="assignment"
+      />
+    </div>
+  </div>
 </template>
+
+<script>
+import AssignementCard from "../../components/subject/AssignmentCard";
+import axios from "axios";
+
+export default {
+  name: "AssignementView",
+  components: {
+    AssignementCard,
+  },
+  async created() {
+    document.title = "QS99 - Subjects";
+
+    //getting subjects assigments
+    await axios
+      .get("http://localhost:8001/exercise/getByUser", {
+        params: {
+          subjectId: 1,
+        },
+      })
+      .then((response) => {
+        this.allAssignments = response.data;
+        console.log(this.allAssignments);
+      });
+  },
+
+  data() {
+    return {
+      allAssignments: [],
+    };
+  },
+};
+</script>

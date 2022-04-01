ab90862aa8be490ede42a95292e1ef69aa8a4a3c
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-04-01 19:10:55 +0200

    getting assignments

--- src/frontend/src/views/subject/AssignmentsView.vue
+++ src/frontend/src/views/subject/AssignmentsView.vue
@@ -1,12 +1,10 @@
 <template>
   <h1>Assignments</h1>
 
-  <div v-for="(entry, index) in queue" :key="entry" class="entry">
+  <div v-for="(entry, index) in assignments" :key="entry" class="entry">
     <label>{{ index + 1 }}</label>
 
-    {{ entry.lastname }}, {{ entry.firstname }}
-
-    | Øving {{ entry.assignment }}
+    {{ entry }}
   </div>
 </template>
 <script>
@@ -16,18 +14,25 @@ export default {
   props: ["subject"],
 
   created() {
-    axios.get("http://localhost:8001/subject/getByUser", {
-      params: {
-        id: this.subject.id,
-      },
-    });
+    axios
+      .get("http://localhost:8001/subject/getByUser", {
+        params: {
+          id: this.subject.id,
+        },
+      })
+      .then((response) => {
+        this.assignments = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 
   methods: {},
 
   data() {
     return {
-      queue: null,
+      assignments: null,
     };
   },
 };

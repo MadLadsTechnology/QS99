e43558aea3db5d7d23c2f74d9d4a93496cfbf23f
Anders Heftøy Carlsen <anders.heftoy@gmail.com>
2022-04-01 10:11:48 +0200

    Added: ManyToMany relation on the student side of student_exercises

--- src/main/java/ntnu/idatt2105/madlads/FullstackAPI/model/users/Student.java
+++ src/main/java/ntnu/idatt2105/madlads/FullstackAPI/model/users/Student.java
@@ -1,6 +1,7 @@
 package ntnu.idatt2105.madlads.FullstackAPI.model.users;
 
 import ntnu.idatt2105.madlads.FullstackAPI.model.subjects.Entry;
+import ntnu.idatt2105.madlads.FullstackAPI.model.subjects.Exercise;
 import ntnu.idatt2105.madlads.FullstackAPI.model.subjects.Subject;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -13,13 +14,20 @@ import java.util.Set;
 
 @Entity
 public class Student extends QSUser {
+    //List of all subjects the student is a student in.
     @ManyToMany(mappedBy = "students", cascade =  CascadeType.REMOVE)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Set<Subject> studentSubjects = new HashSet<>();
 
+    //List of all subjects the student is an assistant in.
     @ManyToMany(mappedBy = "students", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Set<Subject> assitantSubjects = new HashSet<>();
+
+    //List of all approved exercises of a student across all subjects.
+    @ManyToMany(mappedBy = "students", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Set<Exercise> approvedExercises = new HashSet<>();
 
     @ManyToOne
     private Entry entry;
@@ -47,5 +55,12 @@ public class Student extends QSUser {
         assitantSubjects.add(subject);
     }
 
+    public void addApprovedExercise(Exercise exercise){
+        approvedExercises.add(exercise);
+    }
+
+    public Set<Exercise> getApprovedExercises(){
+        return approvedExercises;
+    }
 
 }

66cb7d0adedc684c2c01ef346eed132f6ca5ad92
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-04-01 10:16:03 +0200

    added: delete user

--- src/frontend/src/views/admin/AllUser.vue
+++ src/frontend/src/views/admin/AllUser.vue
@@ -2,8 +2,12 @@
   <h3>All users</h3>
 
   <router-link to="/register">Register user</router-link>
-  <div v-for="student in students" :key="student">
-    {{ student }}
+  <div v-for="user in users" :key="user" class="user">
+    <p>{{ user.emailAddress }}</p>
+    <p>{{ user.lastname }}</p>
+    <p>{{ user.firstname }}</p>
+
+    <button @click="deleteUser(user)">Delete</button>
   </div>
 </template>
 <script>
@@ -11,17 +15,33 @@ import { authComputed } from "@/store/helpers";
 import axios from "axios";
 export default {
   name: "HomeView",
+  methods: {
+    deleteUser(user) {
+      axios
+        .delete("http://localhost:8001/user/delete", {
+          params: {
+            email: user.email,
+          },
+        })
+        .then((response) => {
+          this.users = response.data;
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    },
+  },
   components: {},
   created() {
     document.title = "QS99 - Students";
     //getting subjects of the user
     axios.get("http://localhost:8001/user/getAllUsers").then((response) => {
-      this.students = response.data;
+      this.users = response.data;
     });
   },
   data() {
     return {
-      students: null,
+      users: null,
     };
   },
   computed: {

60fc611c5ce081cab9ac1779b76fb9335e0d315a
Anders Heftøy Carlsen <anders.heftoy@gmail.com>
2022-04-04 18:14:56 +0200

    Changed: return of Get Exercise By Student

--- src/main/java/ntnu/idatt2105/madlads/FullstackAPI/controller/ExerciseController.java
+++ src/main/java/ntnu/idatt2105/madlads/FullstackAPI/controller/ExerciseController.java
@@ -2,6 +2,7 @@ package ntnu.idatt2105.madlads.FullstackAPI.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import ntnu.idatt2105.madlads.FullstackAPI.dto.GetExerciseDTO;
+import ntnu.idatt2105.madlads.FullstackAPI.dto.SubListByUserDTO;
 import ntnu.idatt2105.madlads.FullstackAPI.model.repositories.*;
 import ntnu.idatt2105.madlads.FullstackAPI.model.subjects.Exercise;
 import ntnu.idatt2105.madlads.FullstackAPI.model.subjects.ExerciseSubList;
@@ -173,7 +174,7 @@ public class ExerciseController {
     @Operation(summary = "Get exercise per user", description = "Gets all exercises for a user within one subject")
     @GetMapping("/getByUser")
     @ResponseStatus(value = HttpStatus.CREATED)
-    public ResponseEntity<ArrayList<GetExerciseDTO>> getExercisesByUser(Authentication authentication,
+    public ResponseEntity<ArrayList<SubListByUserDTO>> getExercisesByUser(Authentication authentication,
                                                                         @RequestParam("subjectId") final int subjectId) {
         try {
             if (authentication != null) {
@@ -182,15 +183,23 @@ public class ExerciseController {
                     Subject subject = subjectRepository.findById(subjectId);
                     logger.info("Getting exercises for " + student.getEmailAddress() + " in subject " + subject.getSubjectCode());
                     ArrayList<Exercise> exercises = (ArrayList<Exercise>) exerciseRepository.findExerciseBySubject(subject);
-                    ArrayList<GetExerciseDTO> response = new ArrayList<>();
+                    ArrayList<Long> subListIds = new ArrayList<>();
+                    ArrayList<SubListByUserDTO> response = new ArrayList<>();
                     for (Exercise exercise : exercises) {
-                        if (student.getApprovedExercises().contains(exercise)) {
-                            GetExerciseDTO getExerciseDTO = new GetExerciseDTO(exercise, true);
-                            response.add(getExerciseDTO);
-                        } else {
-                            GetExerciseDTO getExerciseDTO = new GetExerciseDTO(exercise, false);
-                            response.add(getExerciseDTO);
+                        Long id = exercise.getSubList().getId();
+                        if (!subListIds.contains(id)) {
+                            subListIds.add(id);
                         }
+                    }
+                    for(Long id: subListIds){
+                        ExerciseSubList subList = exerciseSubListRepository.findExerciseSubListById(id);
+                        SubListByUserDTO subListByUserDTO =
+                                new SubListByUserDTO(
+                                        subList,
+                                        student,
+                                        exerciseRepository.findExerciseBySubList(subList)
+                                );
+                        response.add(subListByUserDTO);
                     }
                     return new ResponseEntity<>(response, HttpStatus.OK);
                 }
--- src/main/java/ntnu/idatt2105/madlads/FullstackAPI/model/repositories/ExerciseSubListRepository.java
+++ src/main/java/ntnu/idatt2105/madlads/FullstackAPI/model/repositories/ExerciseSubListRepository.java
@@ -8,5 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ExerciseSubListRepository extends JpaRepository<ExerciseSubList, Long> {
+    ExerciseSubList findExerciseSubListById(Long id);
 
 }

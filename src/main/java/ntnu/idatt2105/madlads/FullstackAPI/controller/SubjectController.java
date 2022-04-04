6c67a65e99e6691817f7a0272ad52603944de3d4
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-04-04 21:12:12 +0200

    tried to test frontend and fixed beckend bug

--- src/main/java/ntnu/idatt2105/madlads/FullstackAPI/controller/SubjectController.java
+++ src/main/java/ntnu/idatt2105/madlads/FullstackAPI/controller/SubjectController.java
@@ -229,11 +229,12 @@ public class SubjectController {
                     String lastName = strings[0];
                     String firstName = strings[1];
                     String email = strings[2].trim();
-                    QSUser user = userRepository.getDistinctByEmailAddress(email);
 
                     if (!pattern.matcher(email).matches()) {
                         return new ResponseEntity<>(false, HttpStatus.UNPROCESSABLE_ENTITY);
                     }
+
+                    QSUser user = userRepository.getDistinctByEmailAddress(email);
 
                     if (user == null) {
                         String password = generateCommonLangPassword();
@@ -245,10 +246,10 @@ public class SubjectController {
                     }
                     logger.info("user: " + user);
                     if (user instanceof Student) {
-                        if (!((Student) user).getStudentSubjects().contains(subjectId)) {
-                            ((Student) user).addStudentSubject(subject);
-                        } else {
-                            Student newStudent = new Student();
+                        boolean response = studentRepository.findByEmailAddress(email).addStudentSubject(subject);
+                        if (response) {
+                            logger.info(subject.toString());
+                            subjectRepository.save(subject);
                         }
                     } else if (user instanceof Professor) {
                         if (!subject.getProfessors().contains(user)) {
